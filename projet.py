f6ea94a32a3fe65251d15a37b91f6a35b02acbe4
FORNER Yann <yann.forner@etu.univ-amu.fr>
2021-11-30 15:40:47 +0100

    ajout de la sauvegarde d'un dataframe en csv

--- projet.py
+++ projet.py
@@ -4,9 +4,10 @@ import loader
 #Pour load toutes les pages loader.loadAll(9393)
 
 import time
-df = loader.loadPages(9393, 2)
+df = loader.loadCsv("data/9393.csv")
 
 text = nltk.word_tokenize(df["Commentaire"][3])
+print(df["Commentaire"][3])
 adjectifs = []
 for word, pos in nltk.pos_tag(text):
     if pos in ["NN"]:
