5ceb457bf1d126286812a23601c1881eae3acf4e
QuentinBeauchet <quentinbeauchet@free.fr>
2021-11-30 15:47:44 +0100

    ...

--- projet.py
+++ projet.py
@@ -1,14 +1,36 @@
+from itertools import count
 import nltk
 import loader
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import dataMaker
 
-#Pour load toutes les pages loader.loadAll(9393)
+df = loader.loadPages(9393, 5)
 
-import time
-df = loader.loadPages(9393, 2)
+sid = SentimentIntensityAnalyzer()
 
-text = nltk.word_tokenize(df["Commentaire"][3])
-adjectifs = []
-for word, pos in nltk.pos_tag(text):
-    if pos in ["NN"]:
-        adjectifs.append(word)
-print(adjectifs)
+neg, neu, pos, compound, sentiment = [],[],[],[],[]
+for index,row in df.iterrows():
+    score = sid.polarity_scores(row["Commentaire"])
+    neg.append(score.get("neg"))
+    neu.append(score.get("neu"))
+    pos.append(score.get("pos"))
+    compound.append(score.get("compound"))
+    if score.get("compound") >= 0.5:
+        sentiment.append("positif")
+    elif score.get("compound") <= -0.5:
+        sentiment.append("negatif")
+    else:
+        sentiment.append("neutre")
+
+df["neg"] = neg
+df["neu"] = neu
+df["pos"] = pos
+df["compound"] = compound
+df["sentiment"] = sentiment
+
+print(df)
+
+dataMaker.readData()
+for index,row in df.iterrows():
+    dataMaker.addData(row["Commentaire"])
+dataMaker.writeData()
