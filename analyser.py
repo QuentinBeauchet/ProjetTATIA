345a6f175296940c9395924a5c372a328285b334
QuentinBeauchet <quentinbeauchet@free.fr>
2021-12-28 20:43:27 +0100

    jupiter

--- analyser.py
+++ analyser.py
@@ -1,95 +1,131 @@
-from nltk import RegexpTokenizer
+from sklearn.metrics import classification_report
+from textblob_fr import PatternTagger, PatternAnalyzer
+from textblob import Blobber
+from tqdm import tqdm
+from pandas.core.frame import DataFrame
 import pandas as pd
-from nltk.stem.snowball import FrenchStemmer
+import re
+from spacy.lang.fr.stop_words import STOP_WORDS
 
 
-def initData():
-    df = pd.read_csv("data/FEEL.csv")
-    polarityData = {}
-    for index, row in df.iterrows():
-        polarityData[row["word"]] = row["polarity"]
-    return polarityData
+tb = Blobber(pos_tagger=PatternTagger(), analyzer=PatternAnalyzer())
 
 
-polarityData = initData()
-tokenizer = RegexpTokenizer(r'''\w'|\w+|[^\w\s]''')
-stemmer = FrenchStemmer()
+def cleanup(text):
+    text = text.lower()
 
+    Word_Tok = []
+    for word in re.sub("\W", " ", text).split():
+        Word_Tok.append(word)
 
-def getPolarity(text):
-    textTokenized = detectNegative(tokenizer.tokenize(text))
-    polarityTotal = 0
-    for word in textTokenized:
-        polarityTotal = polarityTotal + calculatePolarity(word)
-    if polarityTotal > 0:
-        return "positive"
-    if polarityTotal < 0:
-        return "negative"
-    return "neutral"
-
-
-def setSentiment(df):
-    sentiment = []
-    for index, row in df.iterrows():
-        sentiment.append(getPolarity(row["Commentaire"]))
-    df["sentiment"] = sentiment
-
-
-def checkResults(df):
-    TP, TN, FP, FN, NEU = 0, 0, 0, 0, 0
-    for index, row in df.iterrows():
-        note = float(row["Note"])
-        if row['sentiment'] == "positive":
-            if note >= 2.5:
-                TP += 1
-            else:
-                FP += 1
-        elif row['sentiment'] == "negative":
-            if note < 2.5:
-                TN += 1
-            else:
-                FN += 1
+    stop_words = set(STOP_WORDS)
+    deselect_stop_words = ['n', 'ne', 'non' 'pas', 'plus',
+                           'personne', 'aucun', 'ni', 'aucune', 'rien']
+    for w in deselect_stop_words:
+        if w in stop_words:
+            stop_words.remove(w)
         else:
-            NEU += 1
-    noteFilm = round(((TP + FP - TN*6.4 - FN - NEU*0.9) /
-                     (TP + FP + TN*6.4 + FN + NEU*0.9)) * 5, 3)
-    precision = round(((TP + TN)/(TP + TN + FP + FN + NEU))*100, 2)
-    return (TP, TN, FP, FN, NEU, noteFilm, precision)
+            continue
+
+    filteredComment = [w for w in Word_Tok if not (
+        (w in stop_words) or (len(w) == 1))]
+
+    return ' '.join(filteredComment)
 
 
-def calculatePolarity(word):
-    negative = False
-    if "NOT_" in word:
-        negative = True
-        word = word.split("_")[1]
-    if word in polarityData.keys():
-        polarity = polarityData[word]
-        if polarity == "positive":
-            if negative:
-                return -1
-            return 1
-        if polarity == "negative":
-            if negative:
-                return 1
-            return -1
-    return 0
+def getClasseFromComment(text):
+    polarity = tb(cleanup(text)).sentiment[0]
+    if polarity > 0:
+        return "Positive"
+    if polarity < 0:
+        return "Negative"
+    return "Neutral"
 
 
-def detectNegative(textTokenized):
-    negationWords = ["pas", "plus"]
-    for i in range(len(textTokenized)):
-        word = stemmer.stem(textTokenized[i])
-        textTokenized[i] = word
-        if (textTokenized[i] in negationWords):
-            textTokenized[i] = "NOT_" + textTokenized[i]
-            if i < len(textTokenized) - 1:
-                textTokenized[i + 1] = "NOT_" + textTokenized[i + 1]
-            if i > 0:
-                textTokenized[i - 1] = "NOT_" + textTokenized[i - 1]
-    return textTokenized
+def getClasseFromNote(note):
+    if note > 3:
+        return "Positive"
+    if note < 2.5:
+        return "Negative"
+    return "Neutral"
 
 
-def analyse(idFilm):
+def analyseMovie(idFilm):
     df = pd.read_csv(f"data/{idFilm}.csv")
-    setSentiment(df)
-    return checkResults(df)
+    y_true = df["Note"].apply(getClasseFromNote)
+    y_pred = df["Commentaire"].apply(getClasseFromComment)
+    return classification_report(y_true, y_pred, output_dict=True), y_pred.value_counts()
+
+
+def analyseAll():
+    df = pd.read_csv("data/data.csv")
+    Note_, ClasseN_, ClasseC_, Difference_, PrecisionP_, PrecisionN_, PrecisionNEU_, PrecisionG_, PrecisionW_ = [
+    ], [], [], [], [], [], [], [], []
+    for index, id in tqdm(df["ID"].items(), total=len(df.index), desc='Analyse'):
+        report, count = analyseMovie(id)
+        nbrPos, nbrNeg, nbrNeu = count
+
+        Note_.append(round(((nbrPos*1.3 - nbrNeg - nbrNeu*0.1) /
+                     (nbrPos*1.3 + nbrNeg + nbrNeu*0.1)) * 5, 2))
+        ClasseN_.append(getClasseFromNote(Note_[index]))
+        if(nbrPos > nbrNeg*3.6):
+            ClasseC_.append("Positive")
+        elif(nbrPos < nbrNeg*2.1):
+            ClasseC_.append("Negative")
+        else:
+            ClasseC_.append("Neutral")
+        Difference_.append(round(Note_[index] - df["Note"][index], 2))
+        PrecisionP_.append(round(report["Positive"]["precision"]*100, 2))
+        PrecisionN_.append(round(report["Negative"]["precision"]*100, 2))
+        PrecisionNEU_.append(round(report["Neutral"]["precision"]*100, 2))
+        PrecisionG_.append(round(report["macro avg"]["precision"]*100, 2))
+        PrecisionW_.append(round(report["weighted avg"]["precision"]*100, 2))
+    df["Note Estimé"] = Note_
+    df["Difference Note"] = Difference_
+    df["Classe Estimé Note"] = ClasseN_
+    df["Classe Estimé Commentaires"] = ClasseC_
+    df["Precision Positive"] = PrecisionP_
+    df["Precision Negative"] = PrecisionN_
+    df["Precision Neutral"] = PrecisionNEU_
+    df["Precision Global"] = PrecisionG_
+    df["Precision Weighted"] = PrecisionW_
+    df.to_csv("data/analysis.csv", index=False, encoding='utf-8')
+    return df
+
+
+def classeStats(df, methode):
+    report = classification_report(df["Classe"], df[methode], output_dict=True)
+    return {"Precision Positive": round(report['Positive']['precision']*100, 2),
+            "Precision Neutral": round(report['Neutral']['precision']*100, 2),
+            "Precision Negative": round(report['Negative']['precision']*100, 2),
+            "Precision Global": round(report['macro avg']['precision']*100, 2),
+            "Precision Weighted": round(report['weighted avg']['precision']*100, 2)}
+
+
+def analyseResults(df):
+    # Stats de determination des classes des commentaires
+    stats = DataFrame({"Precision Positive": [df["Precision Positive"].mean(), df["Precision Positive"].median(), df["Precision Positive"].min(), df["Precision Positive"].max()],
+                       "Precision Negative": [df["Precision Negative"].mean(), df["Precision Negative"].median(), df["Precision Negative"].min(), df["Precision Negative"].max()],
+                       "Precision Neutral": [df["Precision Neutral"].mean(), df["Precision Neutral"].median(), df["Precision Neutral"].min(), df["Precision Neutral"].max()],
+                       "Precision Global": [df["Precision Global"].mean(), df["Precision Global"].median(), df["Precision Global"].min(), df["Precision Global"].max()],
+                       "Precision Weighted": [df["Precision Weighted"].mean(), df["Precision Weighted"].median(), df["Precision Weighted"].min(), df["Precision Weighted"].max()],
+                       "Difference Note": [df["Difference Note"].abs().mean(), df["Difference Note"].abs().median(), df["Difference Note"].abs().min(), df["Difference Note"].abs().max()]},
+                      index=['Moyenne',
+                             'Median',
+                             'Min',
+                             'Max'])
+    stats.to_csv("data/statsComment.csv")
+    print("\nStatistiques sur la determination de la classe des commentaires:\n", stats)
+
+    # Stats de determination des classes des films
+    statsClasse = DataFrame([classeStats(df, "Classe Estimé Note"), classeStats(df, "Classe Estimé Commentaires")],
+                            index=["Classe Estimé Note", "Classe Estimé Commentaires"])
+    statsClasse.to_csv("data/statsClasse.csv")
+    print("\nStatistiques sur la determination de la classe des films:\n", statsClasse)
+
+
+if __name__ == "__main__":
+    df = analyseAll()
+    print(df[["Nom", "Nombre Commentaires", "Note", "Note Estimé", "Difference Note", "Classe",
+          "Classe Estimé Note", "Classe Estimé Commentaires", "Precision Global", "Precision Weighted"]])
+    analyseResults(df)
--- dataMaker.py
+++ dataMaker.py
@@ -6,28 +6,28 @@ import concurrent.futures
 from math import ceil
 from tqdm import tqdm
 
-import analyser
+from analyser import getClasseFromNote
 
 
 def loadPages(idFilm, nom=""):
-    n = ceil(int("".join(loadPage(
+    n = ceil(int("".join(loadHtml(
         f"https://www.allocine.fr/film/fichefilm-{idFilm}/critiques/spectateurs").find_all(
         'h2', class_='titlebar-title titlebar-title-md')[0].text.split()[:-2]))/15)
     urls = ["https://www.allocine.fr/film/fichefilm-%s/critiques/spectateurs/?page=%s" %
             (idFilm, x) for x in range(n)]
     df = DataFrame(columns=["Note", "Commentaire"])
-    with concurrent.futures.ThreadPoolExecutor(max_workers=3) as pool:
-        results = pool.map(loadPage, urls)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=None) as pool:
+        results = pool.map(loadHtml, urls)
         for index, html in tqdm(enumerate(results), total=n, desc=nom):
             for el in html.find_all('div', class_="review-card-review-holder"):
                 df = df.append({
                     "Note": float(el.find('span', {'class': 'stareval-note'}).text.replace(',', '.')),
                     "Commentaire": el.find('div', class_="review-card-content").text.replace('\n', ' ')}, ignore_index=True)
     df.to_csv(f"data/{idFilm}.csv", index=False, encoding='utf-8')
-    return round(df["Note"].mean(), 3)
+    return (round(df["Note"].mean(), 3), len(df.index))
 
 
-def loadPage(httpStr):
+def loadHtml(httpStr):
     r = requests.get(url=httpStr, headers={'Accept-Encoding': 'identity'})
     return BeautifulSoup(r.text, features="html.parser")
 
@@ -37,44 +37,12 @@ def fetchData():
     df = DataFrame(columns=["Nom", "ID", "Note"])
     for index, id in tqdm(data["ID"].items(), total=len(data.index), desc='Total'):
         nom = data["Nom"][index]
-        df = df.append({"Nom": nom, "ID": id,
-                       "Note": loadPages(id, nom)}, ignore_index=True)
+        note, nbrCommentaires = loadPages(id, nom)
+        df = df.append(
+            {"Nom": nom, "ID": id, "Nombre Commentaires": nbrCommentaires, "Note": note, "Classe": getClasseFromNote(note)}, ignore_index=True)
     df.to_csv("data/data.csv", index=False, encoding='utf-8')
     return df
 
 
-def loadData():
-    df = pd.read_csv("data/data.csv")
-    TP_, FP_, TN_, FN_, NEU_, Note_, Difference_, Precision_ = [], [], [], [], [], [], [], []
-    for index, id in tqdm(df["ID"].items(), total=len(df.index), desc='Analyse'):
-        TP, TN, FP, FN, NEU, note, precision = analyser.analyse(id)
-        TP_.append(TP)
-        TN_.append(TN)
-        FP_.append(FP)
-        FN_.append(FN)
-        NEU_.append(NEU)
-        Note_.append(note)
-        Difference_.append(round(note - df["Note"][index], 3))
-        Precision_.append(precision)
-    df["TP"] = TP_
-    df["FP"] = FP_
-    df["TN"] = TN_
-    df["FN"] = FN_
-    df["NEU"] = NEU_
-    df["Note Estimé"] = Note_
-    df["Difference"] = Difference_
-    df["Precision"] = Precision_
-    df.to_csv("data/analysis.csv", index=False, encoding='utf-8')
-    print(df)
-
-    stats = DataFrame({"Precision": [df["Precision"].mean(), df["Precision"].median(), df["Precision"].min(), df["Precision"].max()],
-                       "Difference": [df["Difference"].abs().mean(), df["Difference"].abs().median(), df["Difference"].abs().min(), df["Difference"].abs().max()]},
-                      index=['Moyenne',
-                             'Median',
-                             'Min',
-                             'Max'])
-    print(stats)
-
-
-# fetchData()
-loadData()
+if __name__ == "__main__":
+    fetchData()
